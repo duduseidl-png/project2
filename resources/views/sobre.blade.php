937718e8aca3e29a9d47036ebf8646d41b2e6833
duduseidl-png <duduseidl@gmail.com>
2026-05-07 21:49:18 -0300

    Commit2 07.05

--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -1,4 +1,5 @@
 <x-layout title="Sobre">
-    <h1 class="mb-4">Sobre Nós</h1>
-    <p>Este simulador é resultado de um projeto de estágio obrigatório desenvolvido por uma dupla de acadêmicos do curso de Engenharia de Computação da Universidade Feevale. O sistema foi projetado com o objetivo de auxiliar a comunidade acadêmica na preparação para o Exame Nacional de Desempenho de Estudantes (ENADE), unindo tecnologia e educação.</p>
+    <div class="max-w-3xl mx-auto px-4 py-6">
+        <h1 class="indent-6 text-justify">Este simulador é resultado de um projeto de estágio obrigatório desenvolvido por uma dupla de acadêmicos do curso de Engenharia de Computação da Universidade Feevale. O sistema foi projetado com o objetivo de auxiliar a comunidade acadêmica na preparação para o Exame Nacional de Desempenho de Estudantes (ENADE), unindo tecnologia e educação.</h1>
+    </div>
 </x-layout>
